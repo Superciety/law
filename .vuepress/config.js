7ac59a497813b5f4c02f90207054d13608f9c002
micha vie <micha@freeiam.com>
2022-01-20 00:41:00 +0100

    add prettier config

--- .vuepress/config.js
+++ .vuepress/config.js
@@ -1,35 +1,35 @@
 module.exports = {
-  title: "Laws",
+  title: 'Laws',
   themeConfig: {
-    logo: "/images/logo.png",
-    nav: [{ text: "Superciety.com", link: "https://superciety.com" }],
+    logo: '/images/logo.png',
+    nav: [{ text: 'Superciety.com', link: 'https://superciety.com' }],
     sidebar: [
       {
-        title: "General",
+        title: 'General',
         collapsable: false,
-        children: prefix("general", ["modifications", "super"])
-      }
+        children: prefix('general', ['modifications', 'super']),
+      },
     ],
     smoothScroll: true,
-    lastUpdated: "Last Updated",
-    docsBranch: "main",
-    repo: "superciety/law",
-    editLinks: true
+    lastUpdated: 'Last Updated',
+    docsBranch: 'main',
+    repo: 'superciety/law',
+    editLinks: true,
   },
   plugins: [
-    "@vuepress/active-header-links",
-    "@vuepress/back-to-top",
-    "@vuepress/nprogress",
+    '@vuepress/active-header-links',
+    '@vuepress/back-to-top',
+    '@vuepress/nprogress',
     [
-      "@vuepress/pwa",
+      '@vuepress/pwa',
       {
         serviceWorker: true,
-        updatePopup: true
-      }
-    ]
-  ]
-};
+        updatePopup: true,
+      },
+    ],
+  ],
+}
 
 function prefix(prefix, children) {
-  return children.map(child => `${prefix}/${child}`);
+  return children.map(child => `${prefix}/${child}`)
 }
